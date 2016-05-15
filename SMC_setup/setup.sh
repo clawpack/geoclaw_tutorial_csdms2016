cb0e9ddefd24546537fe3f09b9b58eff3e450f31
Randy LeVeque on SMC <rj@uw.edu>
2016-05-15 06:05:27 +0000

    fixed SMC_setup/setup.sh after moving files

--- SMC_setup/setup.sh
+++ SMC_setup/setup.sh
@@ -6,7 +6,7 @@
 # export statements at end to set enviornment variables.
 # Then any new terminal you open will have these variables set.
 
-cat $HOME/myclaw/set_environment_variables >> $HOME/.bashrc
+cat $HOME/SMC_setup/set_environment_variables >> $HOME/.bashrc
 source $HOME/.bashrc
 cd $CLAW
 python setup.py symlink-only
